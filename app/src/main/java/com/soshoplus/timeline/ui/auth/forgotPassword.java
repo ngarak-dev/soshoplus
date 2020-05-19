037e00d172c11e32136e575477717c9907071d85
Ngara K <ngarakiringo@gmail.com>
2020-05-19 12:23:03 +0300

    default theme changed & reset password activity

---
+++ app/src/main/java/com/soshoplus/timeline/ui/auth/forgotPassword.java
@@ -0,0 +1,13 @@
+package com.soshoplus.timeline.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class forgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}

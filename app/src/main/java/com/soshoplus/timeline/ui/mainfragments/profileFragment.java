adf20b1e25cd1a144bcf807407f2297d92a04006
Ngara K <ngarakiringo@gmail.com>
2020-05-29 10:32:42 +0300

    added views to profile fragment

--- app/src/main/java/com/soshoplus/timeline/ui/mainfragments/profileFragment.java
+++ app/src/main/java/com/soshoplus/timeline/ui/mainfragments/profileFragment.java
@@ -7,12 +7,11 @@
 package com.soshoplus.timeline.ui.mainfragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.soshoplus.timeline.R;
 
@@ -24,7 +23,6 @@ public class profileFragment extends Fragment {
     public profileFragment () {
         // Required empty public constructor
     }
-    
     
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {

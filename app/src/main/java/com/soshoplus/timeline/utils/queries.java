ed0f98ac72ee4ed2ce3be54d58f0984468507c84
Ngara K <ngarakiringo@gmail.com>
2020-06-01 21:16:17 +0300

    added groups queries

--- app/src/main/java/com/soshoplus/timeline/utils/queries.java
+++ app/src/main/java/com/soshoplus/timeline/utils/queries.java
@@ -16,17 +16,40 @@ import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface queries {
+    /*Sign In*/
     @FormUrlEncoded
     @POST("auth")
     Call<accessToken> signIn (@Field("server_key") String server_key, @Field("username") String username, @Field("password") String password);
-    
+    /*Create Account*/
     @FormUrlEncoded
     @POST("create-account")
     Call<accessToken> createAccount (@Field("server_key") String server_key, @Field("email") String email, @Field("username") String username, @Field("password") String password, @Field("confirm_password") String confirm_password, @Field("phone_num") String phone_num);
-    
+    /*Get User Data*/
     @FormUrlEncoded
     @POST("get-user-data")
     Call<userInfo> getUserData (@Query("access_token") String access_token,
                                 @Field("server_key") String server_key,
                                 @Field("fetch") String fetch, @Field("user_id") String user_id);
+    
+    /*GROUPS*/
+    /*Recommended Groups*/
+    @FormUrlEncoded
+    @POST("fetch-recommended")
+    Call<Object> getRecommended(@Query("access_token") String access_token,
+                                @Field("server_key") String server_key,
+                                @Field("fetch") String fetch);
+    
+    /*Joined Groups*/
+    @FormUrlEncoded
+    @POST("get-my-groups")
+    Call<Object> getJoinedGroups(@Query("access_token") String access_token,
+                                @Field("server_key") String server_key,
+                                @Field("fetch") String fetch, @Field("user_id") String user_id);
+    
+    /*My Groups*/
+    @FormUrlEncoded
+    @POST("get-my-groups")
+    Call<Object> getMyGroups(@Query("access_token") String access_token,
+                             @Field("server_key") String server_key,
+                             @Field("fetch") String fetch);
 }
